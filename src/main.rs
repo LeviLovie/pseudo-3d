a833b845724671770ab8e2751a0dab273e9cc13f
leviiloviee <107021730+LeviiLovie@users.noreply.github.com>
2023-06-25 13:14:11 +0700

    Debug mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Booting v{}", env!("CARGO_PKG_VERSION"));
+    // Turn on debug mode, if app rus with '-d' argument
+    let debug = env::args().any(|arg| arg == "--debug");
+
+    // Print information
+    if !debug {println!("Booting v{};",                env!("CARGO_PKG_VERSION"));}
+    else      {println!("Booting v{}, in debug mode;", env!("CARGO_PKG_VERSION"));}
 }
